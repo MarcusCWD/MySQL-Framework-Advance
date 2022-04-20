8d49e029abda48de9522749e718cc70a8e456911
Marcus <marcus.chong@trentglobal.edu.sg>
2022-04-20 02:02:58 +0000

    lab 16

--- routes/users.js
+++ routes/users.js
@@ -44,7 +44,63 @@ router.get('/login', (req,res)=>{
 router.post('/login', async(req,res)=>{
     const loginForm = createLoginForm()
     loginForm.handle(req, {
-        
+        'success': async (form) =>{
+            // similar to mongodb we need to .find to narrow our search
+            let user  = await User.where({    //user here will contain the particular id, user, email, password
+                'email': form.data.email
+            }).fetch({
+                require:false 
+            })
+            // if there is no users of that email from the database, do:
+            if(!user){
+                req.flash('error_messages', 'Sorry, somehting went wrong. Please try again')
+                res.redirect('/users/login')
+            }
+            // if there is such a email from the database, then do:
+            else{
+                // check that the password match from the password field
+                if(user.get('password') === form.data.password){
+                    // login success. email match with the password
+                    // now store the session id with the user information
+                    req.session.user = {
+                        id: user.get('id'),
+                        username: user.get('username'),
+                        email: user.get('email')
+                    }
+                    req.flash('success_message', 'Welcome back ' + user.get('username'))
+                    res.redirect('/users/profile')
+
+                }
+                else{
+                    // login is not successful
+                    // password do not match the password field
+                    req.flash('error_message', 'Sorry, somehting went wrong. Please try again')
+                    res.redirect('/users/login')
+                }
+            }
+        },
+        'error': (form)=>{
+            req.flash('error_message', "There are some problems logging you in. Please fill in the form again")
+            // since the user is currently on the users/login page, we need to 'refresh' the form so 
+            // we use render to render a new form for them
+            res.render('/users/login',{
+                'form': form.toHTML(bootstrapField)
+            })
+        }
+ 
     })
 })
+
+// to do the user's profile
+router.get('/profile', (req,res)=>{
+    // 
+    const user = req.session.user
+
+
+
+    res.render('users/profile.hbs',{
+        'form': loginForm.toHTML(bootstrapField)
+    })
+})
+
 module.exports = router
